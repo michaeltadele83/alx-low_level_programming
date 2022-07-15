593954fc9d44076a0d00ab2ac19250e2d999fb10
Michael Tadele <michaeltadele@gmail.com>
2022-07-15 12:11:43 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,33 +2,25 @@
 
 /**
   * rev_string - Reverses a string
-  * @s: The string to be modified
+  * @s: string to reverse
   *
   * Return: void
   */
 void rev_string(char *s)
 {
 	int i, c, k;
-	char *a, aux;
+	char str[500];
 
-	a = s;
-
-	while (s[c] != '\0')
+	while (*(s + i))
 	{
-		c++;
+		*(str + i) = *(s + i);
+		i++;
 	}
-
-	for (k = 1; k < c; k++)
+	i = i - 1;
+	while (i >= 0)
 	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
+		*(s + i) = *(str + j);
+		j++;
+		i--;
 	}
 }
-
